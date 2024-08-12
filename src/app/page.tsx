bc2c60547a54af10f79fa7dbc4bd068fe0869ee1
md-kamrul <join.kamrul@gmail.com>
2024-08-12 19:19:33 +0600

    hero section running

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h2 className="text-3xl text-center text-yellow-500">music website with next.js and typeScript</h2>
+      <HeroSection></HeroSection>
     </main>
   );
 }
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+
+const HeroSection = () => {
+    return (
+        <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto">
+            Hero Section
+        </div>
+    );
+};
+
+export default HeroSection;
